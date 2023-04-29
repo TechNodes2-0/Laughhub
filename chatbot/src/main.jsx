ddd10bb54f90673ebdecac4766d01b7f535000d7
Nishitbaria <Nishitbaria@gmail.com>
2023-04-29 11:17:03 +0530

    Deploy Web on Github Pages

--- chatbot/src/main.jsx
+++ chatbot/src/main.jsx
@@ -5,18 +5,20 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-y2u1x7z2dud70km5.us.auth0.com"
-      clientId="A1wYMBR5s48dQZx67mzEwHqMNhRS56Tv"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Auth0Provider>
+     <Auth0Provider
+    domain="dev-fkzyzzay6f6jrars.us.auth0.com"
+    clientId="66DaNBfaNuWw4LHYejxIWzE2hOdK4tag"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>
+    
   </React.StrictMode>
 );
