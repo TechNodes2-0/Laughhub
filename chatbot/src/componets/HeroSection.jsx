83af323e0d735e1dd7eac1db9625c512cb924ec2
Nishitbaria <Nishitbaria@gmail.com>
2023-04-30 11:43:16 +0530

    Project : Ready⚡

--- chatbot/src/componets/HeroSection.jsx
+++ chatbot/src/componets/HeroSection.jsx
@@ -19,7 +19,7 @@ const HeroSection = () => {
               to="/Main"
               className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
             >
-              Start Meme BOT
+              Start Ultimate BOT
             </Link>
           </div>
         </div>
