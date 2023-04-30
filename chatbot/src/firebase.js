738857717c6642f0a07c1bca641f831a3ae898bd
Jayesh Yadav <jayeshyadav528@gmail.com>
2023-04-30 10:31:34 +0530

    Update

--- chatbot/src/firebase.js
+++ chatbot/src/firebase.js
@@ -35,6 +35,8 @@ export const requestForToken = () => {
     });
 };
 
+
+
 // Handle incoming messages. Called when:
 // - a message is received while the app has focus
 // - the user clicks on an app notification created by a service worker `messaging.onBackgroundMessage` handler.
